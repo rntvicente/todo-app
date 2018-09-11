97de3306dfcc68720d921da4e6d16f95f0dbc13d
renato.vicente <rntvicente@gmail.com>
2018-09-10 22:53:59 -0300

    Finalização de app

--- frontend/src/todo/todo-form.jsx
+++ frontend/src/todo/todo-form.jsx
@@ -3,15 +3,18 @@ import Grid from '../template/grid'
 import IconButton from '../template/icon-button'
 
 export default props => (
-  <div role='form' className='todo-form'>
+  <div role='form' className='todoForm'>
     <Grid cols="12 9 10">
       <input id='description' className='form-control'
         onChange={props.handleChange}
-        placeholder='Add a task'></input>
+        placeholder='Add a task'
+        value={props.description}></input>
     </Grid>
 
     <Grid cols="12 3 2">
       <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
+      <IconButton style="info" icon="search" onClick={props.handleSearch}></IconButton>
+      <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
     </Grid>
   </div>
 );
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -13,30 +13,76 @@ export default class Todo extends Component {
 
     this.state = { description: '', list: [] };
 
-    this.handleChange = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+
+    this.refresh();
+  };
+
+  refresh(description = '') {
+    const search = (description ? `&description__regex=/${description}/` : '');
+
+    axios.get(`${uri}?sort=-createdAt${search}`)
+      .then(resp => this.setState({ ...this.state, description, list: resp.data }))
   };
 
   handleAdd() {
     const description = this.state.description;
 
     axios.post(uri, { description })
-      .then(res => console.log('yugigyugiyguygugguyguy'))
-      .catch(err => console.error(err));
+      .then(res => this.refresh());
   };
 
   handleChange(event) {
     this.setState({ ...this.state, description: event.target.value });
   }
 
+  handleRemove(todo) {
+    axios.delete(`${uri}/${todo._id}`, { todo })
+      .then(res => this.refresh(this.state.description));
+  };
+
+  handleMarkAsDone(todo) {
+    axios.put(`${uri}/${todo._id}`, { ...todo, done: true })
+      .then(res => this.refresh(this.state.description));
+  };
+
+  handleMarkAsPending(todo) {
+    axios.put(`${uri}/${todo._id}`, { ...todo, done: false })
+      .then(res => this.refresh(this.state.description));
+  };
+
+  handleSearch() {
+    this.refresh(this.state.description);
+  };
+
+  handleClear() {
+    this.refresh();
+  };
+
   render() {
     return (
       <div>
         <PageHeader name='Tasks' small=' Register'></PageHeader>
-        <TodoForm description={this.state.description}
+
+        <TodoForm
+          description={this.state.description}
           handleChange={this.handleChange}
-          handleAdd={this.handleAdd} />
-        <TodoList />
+          handleAdd={this.handleAdd}
+          handleSearch={this.handleSearch}
+          handleClear={this.handleClear} />
+
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending} />
       </div>
     );
   };
