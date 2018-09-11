872c1a66301c8b9e083df98b9c80253346cc7fd0
renato.vicente <rntvicente@gmail.com>
2018-09-10 23:02:02 -0300

    Adicionando comando por teclado

--- frontend/src/todo/todo-form.jsx
+++ frontend/src/todo/todo-form.jsx
@@ -2,19 +2,30 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/icon-button'
 
-export default props => (
-  <div role='form' className='todoForm'>
-    <Grid cols="12 9 10">
-      <input id='description' className='form-control'
-        onChange={props.handleChange}
-        placeholder='Add a task'
-        value={props.description}></input>
-    </Grid>
+export default props => {
+  const keyHandler = (e) => {
+    if (e.key === 'Enter') {
+      e.shiftKey ? props.handleSearch() : props.handleAdd()
+    } else if (e.key === 'Escape') {
+      props.handleClear()
+    }
+  };
 
-    <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
-      <IconButton style="info" icon="search" onClick={props.handleSearch}></IconButton>
-      <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
-    </Grid>
-  </div>
-);
+  return (
+    <div role='form' className='todoForm'>
+      <Grid cols="12 9 10">
+        <input id='description' className='form-control'
+          onChange={props.handleChange}
+          placeholder='Add a task'
+          value={props.description}
+          onKeyUp={keyHandler}></input>
+      </Grid>
+
+      <Grid cols="12 3 2">
+        <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
+        <IconButton style="info" icon="search" onClick={props.handleSearch}></IconButton>
+        <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
+      </Grid>
+    </div>
+  )
+};
